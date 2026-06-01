105e04eeaafffb2d463422f87a2838a53cade118
csmetwge-hash <csmetwge@gmail.com>
2026-06-01 18:02:49 -0400

    Add files via upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,14 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",       // Points to your root app folder
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",// Points to your root components folder
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",     // (Optional) Points to pages if you use them
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
 export default config;
